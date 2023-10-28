d0a0cfb8da40b26660e12b1e6571b744e1628143
BrianJ-4 <84641264+BrianJ-4@users.noreply.github.com>
2023-10-27 20:06:16 -0700

    Update payload2.ps1

--- payload2.ps1
+++ payload2.ps1
@@ -1,3 +1,3 @@
 Function Speak-Text($Text) { Add-Type -AssemblyName System.speech; $TTS = New-Object System.Speech.Synthesis.SpeechSynthesizer; $TTS.Speak($Text) }
-Speak-Text "You look great on your webcam Ha Ha Ha Ha Ha"
+Speak-Text "You look great on your webcam. Ha Ha Ha Ha Ha!"
 Exit
